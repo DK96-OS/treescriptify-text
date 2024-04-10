0a8a7520a8959a1c3ce56d2fc0f942ab2f799d7b
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-04-10 09:36:49 -0400

    Tree Text Reader Quality Improvement (#2)
    Test Treescriptify:
    * Update test_tree_reader.py - add test case assertions, add test cases
    
    Treescriptify:
    * Update tree_reader.py - add internal method calculate_depth, debug internal method process_line, simplify internal method process_line

--- test/treescriptify/test_tree_reader.py
+++ test/treescriptify/test_tree_reader.py
@@ -1,5 +1,6 @@
 """ Testing Tree Reader Methods.
 """
+from treescriptify.tree_node_data import TreeNodeData
 from treescriptify.tree_reader import read_tree_text
 
 
@@ -31,3 +32,111 @@ def test_read_tree_text_single_dir_returns_single_data():
     assert result[0].name == 'my-app'
     assert result[0].depth == 0
     assert result[0].is_dir == True
+
+
+def test_read_tree_text_basic_tree_returns_data():
+    text = '''
+test
+├── __init__.py
+├── input
+│   ├── test_argument_parser.py
+└── treescriptify
+    └── test_tree_reader.py
+    '''
+    result = list(read_tree_text(text))
+    print(result)
+    assert len(result) == 6
+    assert result == [
+        TreeNodeData(0, True, 'test'),
+        TreeNodeData(1, False, '__init__.py'),
+        TreeNodeData(1, True, 'input'),
+        TreeNodeData(2, False, 'test_argument_parser.py'),
+        TreeNodeData(1, True, 'treescriptify'),
+        TreeNodeData(2, False, 'test_tree_reader.py'),
+    ]
+
+
+def test_read_tree_text_complex_tree1_returns_data():
+    text = '''
+test
+├── __init__.py
+├── input
+│   ├── __init__.py
+│   ├── test_argument_parser.py
+│   ├── test_file_validation.py
+│   └── test_string_validation.py
+└── treescriptify
+    ├── test_script_writer.py
+    └── test_tree_reader.py
+    '''
+    result = list(read_tree_text(text))
+    print(result)
+    assert len(result) == 10
+    assert result == [
+        TreeNodeData(0, True, 'test'),
+        TreeNodeData(1, False, '__init__.py'),
+        TreeNodeData(1, True, 'input'),
+        TreeNodeData(2, False, '__init__.py'),
+        TreeNodeData(2, False, 'test_argument_parser.py'),
+        TreeNodeData(2, False, 'test_file_validation.py'),
+        TreeNodeData(2, False, 'test_string_validation.py'),
+        TreeNodeData(1, True, 'treescriptify'),
+        TreeNodeData(2, False, 'test_script_writer.py'),
+        TreeNodeData(2, False, 'test_tree_reader.py'),
+    ]
+
+
+def test_read_tree_text_complex_tree2_returns_data():
+    text = '''com
+└── yourapp
+    ├── ui
+    │   ├── theme
+    │   │   ├── Color.kt
+    │   │   ├── Shape.kt
+    │   │   ├── Theme.kt
+    │   │   └── Type.kt
+    │   │
+    │   ├── atoms
+    │   │   ├── ButtonPrimary.kt
+    │   │   ├── ButtonSecondary.kt
+    │   │   ├── IconLoader.kt
+    │   │   ├── TextInputField.kt
+    │   │   ├── ToggleSwitch.kt
+    │   │   └── TypographyText.kt
+    │   │
+    │   └── pages
+    │       ├── LoginPage.kt
+    │       ├── UserProfilePage.kt
+    │       ├── ArticleListPage.kt
+    │       └── ProductCatalogPage.kt
+    │
+    └── utils
+        └── Extensions.kt
+    '''
+    result = list(read_tree_text(text))
+    print(result)
+    assert len(result) == 22
+    assert result == [
+        TreeNodeData(0, True, 'com'),
+        TreeNodeData(1, True, 'yourapp'),
+        TreeNodeData(2, True, 'ui'),
+        TreeNodeData(3, True, 'theme'),
+        TreeNodeData(4, False, 'Color.kt'),
+        TreeNodeData(4, False, 'Shape.kt'),
+        TreeNodeData(4, False, 'Theme.kt'),
+        TreeNodeData(4, False, 'Type.kt'),
+        TreeNodeData(3, True, 'atoms'),
+        TreeNodeData(4, False, 'ButtonPrimary.kt'),
+        TreeNodeData(4, False, 'ButtonSecondary.kt'),
+        TreeNodeData(4, False, 'IconLoader.kt'),
+        TreeNodeData(4, False, 'TextInputField.kt'),
+        TreeNodeData(4, False, 'ToggleSwitch.kt'),
+        TreeNodeData(4, False, 'TypographyText.kt'),
+        TreeNodeData(3, True, 'pages'),
+        TreeNodeData(4, False, 'LoginPage.kt'),
+        TreeNodeData(4, False, 'UserProfilePage.kt'),
+        TreeNodeData(4, False, 'ArticleListPage.kt'),
+        TreeNodeData(4, False, 'ProductCatalogPage.kt'),
+        TreeNodeData(2, True, 'utils'),
+        TreeNodeData(3, False, 'Extensions.kt'),
+    ]
--- treescriptify/tree_reader.py
+++ treescriptify/tree_reader.py
@@ -7,7 +7,7 @@ from treescriptify.tree_node_data import TreeNodeData
 
 
 def read_tree_text(tree_text: str) -> Generator[TreeNodeData, None, None]:
-    """ Read the JSON string and generate TreeNodeData for all elements.
+    """ Read the Tree Text string and generate TreeNodeData for all elements.
     """
     for line in _split_lines(tree_text):
         if (node := _process_line(line)) is not None:
@@ -26,18 +26,34 @@ def _split_lines(text: str):
 def _process_line(line: str) -> TreeNodeData | None:
     """ Process the Line Text into a Tree Node Data.
     """
-    if line.strip() == "":
-        return None
-    # Determine the depth by counting the number of spaces or special characters before the name
-    depth = (len(line) - len(line.lstrip('│ '))) // 4  # Adjusted based on the given structure
-
     # Extract the name of the file or directory
-    name = line.strip('│ └── ├')
+    if (name := line.strip('│ └── ├\xa0')) == "":
+        return None # Ignore Empty Lines
+    if name.startswith('#') or name.startswith('//'):
+        return None # Ignore comments
+    # Determine directory status
+    if name.endswith('/'):
+        is_dir = True
+        name = name[:-1] # If it's a directory, remove the trailing slash
+    elif name.startswith('.'):
+        # hidden file or dir
+        simple_name = name.lstrip('.')
+        # check for presence of file extension
+        is_dir = '.' not in simple_name
+    elif '.' not in name:
+        # no file extension
+        is_dir = True
+    else:
+        is_dir = False
+    return TreeNodeData(_calculate_depth(line), is_dir, name)
 
-    # Determine if it's a directory
-    is_dir = name.endswith('/')
 
-    # If it's a directory, remove the trailing slash
-    if is_dir:
-        name = name[:-1]
-    return TreeNodeData(depth, is_dir, name)
+def _calculate_depth(line: str) -> int:
+    """Calculate the Depth of a Line in the Tree.
+    """
+    if '──' not in line:
+        return 0
+    # Use Length of Indentation
+    if (i_length := len(line.split('──')[0])) == 0:
+        return 0
+    return (i_length + 3) // 4
